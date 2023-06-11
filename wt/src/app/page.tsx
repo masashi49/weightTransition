1810c464ba584bab6b5fc9c7c4973b028290d9de
yamadamasashi <shisama94@gmail.com>
2023-06-11 17:47:53 +0900

    axe-core入れた

--- wt/src/app/page.tsx
+++ wt/src/app/page.tsx
@@ -1,6 +1,9 @@
-import Image from 'next/image';
+'use client';
+import React from 'react';
+import reportAccessibility from '@/utils/reportAccessibility';
 import { Home } from '@/components/pages/home';
 
 export default function TopPage() {
+  reportAccessibility(React);
   return <Home />;
 }
--- wt/src/components/features/AddWeightDataModal/AddWeightDataModal.test.tsx
+++ wt/src/components/features/AddWeightDataModal/AddWeightDataModal.test.tsx
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+const { axe, toHaveNoViolations } = require('jest-axe');
+
 import { AddWeightDataModal } from './AddWeightDataModal';
 
 describe('Rendering', () => {
-  it('Should render hello text', () => {
-    render(
-      <>
-        <AddWeightDataModal isOpen={true} weightAdd={() => console.log(1)} />
-      </>
+  it('Should render hello text', async () => {
+    const containder = render(
+      <AddWeightDataModal isOpen={false} weightAdd={() => console.log(1)} />
     );
-    expect(screen.getByText('閉じる')).toBeInTheDocument();
+    const accessigbleAxeObject = await axe(containder);
+    expect(accessigbleAxeObject).toHabeNoVioLations();
   });
 });
